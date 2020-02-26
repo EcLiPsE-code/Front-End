53dbc71d1a48dfe3f7717bdacdd1af14aa6ed2c4
Mihail Solodkov <fantazermyname@gmail.com>
2020-02-26 10:55:10 +0300

    bug fixed, lab1 complete

--- Lab1/js/javaScript.js
+++ Lab1/js/javaScript.js
@@ -20,16 +20,6 @@ function currentDate() {
 setInterval(function () {
     document.getElementById('dateDocument').innerHTML = currentDate();
 }, 1000);
-let users = [
-    {id: 1, name: "Вася"},
-    {id: 2, name: "Петя"},
-    {id: 3, name: "Маша"}
-];
-
-let user = users.find(item => item.id == 1);
-
-alert(user.name); // Вася
-
 
 
 
